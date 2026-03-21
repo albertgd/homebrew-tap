6f26f2e05cdfe512388e7d48a6b9dc6dcaf34015
Albert Garcia <albert.diaz@hellofresh.com>
2026-03-21 09:45:05 +0100

    Add markdown-for-mac cask v1.0.0

---
+++ Casks/markdown-for-mac.rb
@@ -0,0 +1,25 @@
+cask "markdown-for-mac" do
+  arch arm: "arm64", intel: "x64"
+
+  version "1.0.0"
+  sha256 arm:   "fe569b9f21a8fb5cba64a3cf7a4d69e23fc475a26affeab80855140d68f3ae05",
+         intel: "b9ce7ca3314766b97fef033dfd4509b03a2e9f621d36ae9396cfbc46118ee3d5"
+
+  url "https://github.com/albertgd/markdown-for-mac/releases/download/v#{version}/Markdown.for.Mac-#{version}-#{arch}.dmg"
+  name "Markdown for Mac"
+  desc "A clean, native markdown viewer for macOS"
+  homepage "https://github.com/albertgd/markdown-for-mac"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Markdown for Mac.app"
+
+  zap trash: [
+    "~/Library/Application Support/markdown-for-mac",
+    "~/Library/Preferences/com.markdownformac.app.plist",
+    "~/Library/Saved Application State/com.markdownformac.app.savedState",
+  ]
+end
