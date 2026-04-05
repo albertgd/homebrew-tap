2e52520243814b049d7daca4d08cbd41dd3ae45a
albertgd <albertgd@gmail.com>
2026-04-05 23:18:43 +0200

    feat: bump librelink-for-mac to v1.1.0

--- Casks/librelink-for-mac.rb
+++ Casks/librelink-for-mac.rb
@@ -1,10 +1,10 @@
 cask "librelink-for-mac" do
-  version "1.0.0"
+  version "1.1.0"
   sha256 :no_check
 
   url "https://github.com/albertgd/librelink-for-mac/releases/download/v#{version}/LibreLinkForMac.zip"
   name "LibreLink HUD for Mac"
-  desc "Menu bar app that displays real-time glucose data from LibreLinkUp"
+  desc "Menu bar app that displays real-time glucose data from LibreLinkUp with floating HUD"
   homepage "https://github.com/albertgd/librelink-for-mac"
 
   depends_on macos: ">= :ventura"
